2e17114e796599c9650c942f60f764fe835134be
STREETFIENDZ <108836264+STREETFIENDZ@users.noreply.github.com>
2022-07-18 18:30:23 -0400

    sss

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -20,7 +20,6 @@ const button_presale_mint_not_whitelisted = "Get on the Whitelist";
 const button_presale_already_minted = "Join The Community";
 const button_public_mint = "Mint Your NFT";
 const mint_failed = "Minting failed. 😢 Please try again.";
-
 const abi = [
     {
       "inputs": [
